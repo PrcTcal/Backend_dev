21756e788a9d38c95ae3b0f721cae7a046976723
PrcTcal <hojjang0826@gmail.com>
2020-11-26 23:11:29 +0900

    first commit

---
+++ src/main/java/com/glen/dev/DevApplication.java
@@ -0,0 +1,18 @@
+package com.glen.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@SpringBootApplication
+public class DevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/com/glen/dev/DevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glen.dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
